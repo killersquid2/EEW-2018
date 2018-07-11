d1d48ebcb848e8ac7bf3f4ff8c649784fb91a0d8
VISCEN012 <VISCEN012@FED.CCLRC.AC.UK>
2018-07-11 13:52:52 +0100

    removed nested folder

--- Alert Arduino/Alert_Final/Alert_Final.ino
+++ Alert Arduino/Alert_Final/Alert_Final.ino
@@ -10,18 +10,28 @@ XBeeResponse response = XBeeResponse();
 ZBRxResponse rx = ZBRxResponse();
 ModemStatusResponse msr = ModemStatusResponse();
 
-LedAlert = 12;
-LedSafe = 13;
+int LedAlert = 12; // red led
+int LedSafe = 13; // green led
+int Buzzer = 10; 
+
+bool Safe;
+
 
 void setup()
 {
   // put your setup code here, to run once:
+
+
+  
   pinMode(LedAlert, OUTPUT);
   pinMode(LedSafe, OUTPUT);
+  pinMode(Buzzer, OUTPUT);
   
   DebugSerial.begin(115200);
   XBeeSerial.begin(9600);
   xbee.begin(XBeeSerial);
+
+
 }
 
 void loop()
@@ -42,9 +52,19 @@ void loop()
       {
         DebugSerial.println("Packet received, but no acknowledgement");
       }
-      for (int i=0; i<rx.getDataLength(); i++){
+      for (int i=0; i<rx.getDataLength(); i++)
+      {
         DebugSerial.print(char(rx.getData(i)));
       }
+      if (rx.getData(0) == 'T')
+      {
+        if (rx.getData(1) == '0')
+        { Safe = true; }
+        if (rx.getData(1) == '1')
+        { Safe = false; }
+        
+      }
+      
       DebugSerial.println();
     }
     else if (xbee.getResponse().getApiId() == MODEM_STATUS_RESPONSE)
@@ -56,7 +76,49 @@ void loop()
   {
     DebugSerial.println("Error reading packet.");
   }
+
+  
+   if (Safe == true)
+   {
+    digitalWrite(LedSafe, HIGH);
+   }
+   if (Safe == false)
+   {
+    digitalWrite(LedSafe, LOW);
+    digitalWrite(LedAlert, HIGH);  
+    delay(50);              
+    digitalWrite(LedAlert, LOW);   
+    delay(50);
+
+   play('B', 2);      
+   play('E', 2);      
+
+   }
+   
+ 
 }
+
+void play(char note, int beats) {
+  int numNotes = 2; 
+  char notes[] = { 'E', 'B', ' '};
+  int frequencies[] = {330, 495, 0};
+  int currentFrequency = 0;
+  int beatLength = 150;
+
+  for (int i = 0; i < numNotes; i++)  // check each value in notes from 0 to 14
+    {
+      if (notes[i] == note)             // does the letter passed to the play function match the letter in the array?
+      {
+       currentFrequency = frequencies[i];   // Yes! Set the current frequency to match that note
+     }
+   }
+
+  //play the frequency that matched our letter for the number of beats passed to the play function
+   tone(Buzzer, currentFrequency, beats * beatLength);   
+   delay(beats* beatLength);   //wait for the length of the tone so that it has time to play
+   delay(50);   //a little delay between the notes makes the song sound more natural
+}
+
 
 
 
