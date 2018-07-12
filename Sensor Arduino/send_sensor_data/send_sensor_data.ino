6ba30f76e457aa463836f43f06cbc3ae3a22c78a
VISCEN018 <VISCEN018@FED.CCLRC.AC.UK>
2018-07-12 13:34:47 +0100

    Cleaned up code

--- Sensor Arduino/send_sensor_data/send_sensor_data.ino
+++ Sensor Arduino/send_sensor_data/send_sensor_data.ino
@@ -20,25 +20,29 @@ XBeeAddress64 addr64 = XBeeAddress64(0x0013a200, 0x4150EC4B);
 ZBTxRequest zbTx = ZBTxRequest(addr64, payload, sizeof(payload));
 ZBTxStatusResponse txStatus = ZBTxStatusResponse();
 
-void setup() {
+void setup()
+{
   // put your setup code here, to run once:
   DebugSerial.begin(115200);
   XBeeSerial.begin(9600);
   xbee.begin(XBeeSerial);
 
   bool status = bme.begin();  
-    if (!status) {
-        Serial.println("Could not find a valid BME280 sensor, check wiring!");
-        while (1);
-    }
+  if (!status)
+  {
+      Serial.println("Could not find a valid BME280 sensor, check wiring!");
+      while (1);
+  }
 }
 
-void loop() {
+void loop()
+{
   // put your main code here, to run repeatedly:
   
   String dataValues = getValues();
 
-  for(int i=0; i<sizeof(payload); i++) {
+  for(int i=0; i<sizeof(payload); i++)
+  {
     payload[i] = dataValues[i];
   }
 
@@ -47,13 +51,14 @@ void loop() {
   delay(5000);
 }
 
-String getValues() {
-    String data = "";
-    char delimiter = ',';
+String getValues()
+{
+  String data = "";
+  char delimiter = ',';
     
-    data += String(bme.readTemperature()) + delimiter;  // Measured in *C
-    data += String(bme.readPressure()/100.0F) + delimiter; // Measured in hPa
-    data += String(bme.readHumidity());  // Measured in %
+  data += String(bme.readTemperature()) + delimiter;  // Measured in *C
+  data += String(bme.readPressure()/100.0F) + delimiter; // Measured in hPa
+  data += String(bme.readHumidity());  // Measured in %
 
-    return data;
+  return data;
 }
